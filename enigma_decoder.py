93e6647236b82496f6650898ab9bf213f032a6e5
jmiguelmangas <jmmangas@gmail.com>
2023-12-08 13:28:09 +0100

    Primera Version Codificadora Funcionando

--- enigma_decoder.py
+++ enigma_decoder.py
@@ -1,42 +1,68 @@
-def descifrar(mensaje_cifrado):
-    
-    # Lista de todas las posibles combinaciones de rotores y cableado
-    rotores = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "BCDEFGHIJKLMNOPQRSTUVWXYZA", "CDEFGHIJKLMNOPQRSTUVWXYZAB"]
-    cableado = [(a, b) for a in range(26) for b in range(26)]
+from string import ascii_uppercase
 
-    # Lista de todas las posibles posiciones de los rotores
-    posiciones = [(a, b, c) for a in range(26) for b in range(26) for c in range(26)]
+# Rotores de la máquina Enigma
+rotor_I = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+rotor_II = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
+rotor_III = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
 
-    # Bucle sobre todas las posibles combinaciones de rotores, cableado y posiciones
-    for rotor1 in rotores:
-        for rotor2 in rotores:
-        for rotor3 in rotores:
-            for posicion1 in posiciones:
-            for posicion2 in posiciones:
-                for posicion3 in posiciones:
+reflector_B = {
+    'A': 'Y', 'B': 'R', 'C': 'U', 'D': 'H', 'E': 'Q', 'F': 'S', 'G': 'L', 'H': 'D',
+    'I': 'P', 'J': 'X', 'K': 'N', 'L': 'G', 'M': 'O', 'N': 'K', 'O': 'M', 'P': 'I',
+    'Q': 'E', 'R': 'B', 'S': 'F', 'T': 'Z', 'U': 'C', 'V': 'W', 'W': 'V', 'X': 'J',
+    'Y': 'A', 'Z': 'T'
+}
 
-                # Descifrar el mensaje cifrado
-                mensaje_descifrado = ""
-                for letra in mensaje_cifrado:
-                    letra_cifrada = letra
-                    for i in range(3):
-                    letra_cifrada = rotores[i][letra_cifrada]
-                    letra_cifrada = cableado[posicion1[i]][letra_cifrada]
-                    letra_cifrada = rotores[i + 1][letra_cifrada]
-                    letra_cifrada = cableado[posicion2[i]][letra_cifrada]
-                    letra_cifrada = rotores[i + 2][letra_cifrada]
-                    letra_cifrada = cableado[posicion3[i]][letra_cifrada]
-                    mensaje_descifrado += letra_cifrada
+def rotor_shift(rotor, shift):
+    return rotor[shift:] + rotor[:shift]
 
-                # Comprobar si las dos últimas palabras son "heil hitler"
-                if mensaje_descifrado[-8:] == "heil hitler":
-                    return mensaje_descifrado
+def enigma(text, rotor1, rotor2, rotor3, reflector, rotor1_pos, rotor2_pos, rotor3_pos):
+    encrypted_text = ""
+    for letter in text:
+        if letter in ascii_uppercase:
+            # Avanzar los rotores
+            rotor1 = rotor_shift(rotor1, 1)
+            if rotor1_pos % 26 == 0:
+                rotor2 = rotor_shift(rotor2, 1)
+                if rotor2_pos % 26 == 0:
+                    rotor3 = rotor_shift(rotor3, 1)
+            
+            # Paso a través del rotor 1
+            index = (ascii_uppercase.index(letter) + rotor1_pos) % 26
+            letter = rotor1[index]
 
-    # Si no se ha encontrado ninguna coincidencia, devolver None
-    return None
+            # Paso a través del rotor 2
+            index = (ascii_uppercase.index(letter) + rotor2_pos - rotor1_pos) % 26
+            letter = rotor2[index]
 
+            # Paso a través del rotor 3
+            index = (ascii_uppercase.index(letter) + rotor3_pos - rotor2_pos) % 26
+            letter = rotor3[index]
 
-mensaje_cifrado = "qwertyuioasdfghjklzxcvbnm heil hitler"
-mensaje_descifrado = descifrar(mensaje_cifrado)
+            # Reflector
+            letter = reflector[letter]
 
-print(mensaje_descifrado)
+            # Paso a través de los rotores en reversa
+            index = (rotor3.index(letter) - rotor3_pos + rotor2_pos) % 26
+            letter = ascii_uppercase[index]
+
+            index = (rotor2.index(letter) - rotor2_pos + rotor1_pos) % 26
+            letter = ascii_uppercase[index]
+
+            index = (rotor1.index(letter) - rotor1_pos) % 26
+            letter = ascii_uppercase[index]
+
+            encrypted_text += letter
+
+            rotor1_pos += 1
+            rotor2_pos += 1
+            rotor3_pos += 1
+    return encrypted_text
+
+# Ejemplo de uso:
+mensaje = "HELLO MY NAME IS JOSE"
+posicion_rotor1 = 0
+posicion_rotor2 = 0
+posicion_rotor3 = 0
+
+mensaje_cifrado = enigma(mensaje, rotor_I, rotor_II, rotor_III, reflector_B, posicion_rotor1, posicion_rotor2, posicion_rotor3)
+print("Mensaje cifrado:", mensaje_cifrado)
